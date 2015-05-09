0c038432b074c811a763dedfd3c9be8daafaad9c
Brigitte Bedard <brigitte.bedard@gmail.com>
2015-05-09 07:53:53 -0400

    Change the table def usr_add -> usr_adr

--- data/CreateTable.sql
+++ data/CreateTable.sql
@@ -4,6 +4,6 @@ CREATE TABLE  `zf2tutorial`.`address_book` (
   `usr_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `usr_email` varchar(75) COLLATE utf8_unicode_ci DEFAULT NULL,
   `usr_tel` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `usr_add` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `usr_adr` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`usr_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
